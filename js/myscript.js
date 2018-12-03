8a0dab6532f028034aeca951b60b8a429ce74504
Luciano <ccll4ever1@gmail.com>
2018-12-03 21:30:00 +0000

    update to the html

---
+++ js/myscript.js
@@ -0,0 +1,58 @@
+// map
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'),{
+    center: {lat: -34.397, lng: 150.644},
+    zoon: 8
+  });
+}
+
+// google sign 
+function onSuccess(googleUser) {
+      console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+    }
+    function onFailure(error) {
+      console.log(error);
+    }
+    function renderButton() {
+      gapi.signin2.render('my-signin2', {
+        'scope': 'profile email',
+        'width': 240,
+        'height': 50,
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': onSuccess,
+        'onfailure': onFailure
+      });
+    }
+    
+    
+
+//  seach box
+
+(function() {
+    var cx = '000839719132049059247:-xxothaal9a';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+
+    
+// search results
+
+
+  (function() {
+    var cx = '000839719132049059247:-xxothaal9a';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+
+
+        
