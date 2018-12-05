c882f54e1b48c1c6b920f144cfcee2b2514ad352
Luciano <ccll4ever1@gmail.com>
2018-12-05 21:41:38 +0000

    google mapas with location marker

--- js/myscript.js
+++ js/myscript.js
@@ -1,34 +1,3 @@
-// map
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'),{
-    center: {lat: 40.782710, lng: -73.965310},
-    zoon: 8
-  });
-}
-
-// google sign
-
-function onSuccess(googleUser) {
-      console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
-    }
-    function onFailure(error) {
-      console.log(error);
-    }
-    function renderButton() {
-      gapi.signin2.render('my-signin2', {
-        'scope': 'profile email',
-        'width': 240,
-        'height': 50,
-        'longtitle': true,
-        'theme': 'dark',
-        'onsuccess': onSuccess,
-        'onfailure': onFailure
-      });
-    }
-    
-    
-
 //  seach box
 
 (function() {
@@ -53,6 +22,54 @@ function onSuccess(googleUser) {
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(gcse, s);
   })();
+
+
+
+// map
+
+function initMap() {
+  var london = {lat: 51.507351, lng: -0.127758}
+  
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: london
+  });
+  var marker = new google.maps.Marker({
+    position: london, 
+    map: map
+  });
+}
+
+// var map;
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById("map"),{
+//     center: {lat: 51.507351, lng: -0.127758},
+//     zoon: 8
+//   });
+// }
+
+// google sign
+
+function onSuccess(googleUser) {
+      console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+    }
+    function onFailure(error) {
+      console.log(error);
+    }
+    function renderButton() {
+      gapi.signin2.render('my-signin2', {
+        'scope': 'profile email',
+        'width': 240,
+        'height': 50,
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': onSuccess,
+        'onfailure': onFailure
+      });
+    }
+    
+    
+
 
 
         
